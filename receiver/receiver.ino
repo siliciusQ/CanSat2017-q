772714c0e626b39a8ef9616b157bbb42274ae4ac
silicius <silicius42@gmail.com>
2017-10-16 16:26:22 +0200

    setup function

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -4,7 +4,7 @@
 #define NETWORK_ID 100
 #define ENCRYPT_KEY "tiro finale"
 
-RFM69 radio
+RFM69 radio;
 
 void setup() {
   Serial.begin(9600);
@@ -17,6 +17,17 @@ void setup() {
 }
 
 void loop() {
-  
+
+  if (radio.receiveDone()) {
+    // print additional info
+    Serial.println(" < ---  Receive done:  --- >");
+    Serial.print("Sender ID:\t"); Serial.print(String(radio.SENDERID)); Serial.print("\n");
+    Serial.print("Strength:\t"); Serial.print(String(radio.RSSI)); Serial.print("\n");
+    Serial.println();
+    //print actual message
+    for (byte i=0; i < radio.DATALEN; i++) {
+      Serial.print((char)radio.DATA[i]); //print every byte of message
+    }
+  }
 
 }
