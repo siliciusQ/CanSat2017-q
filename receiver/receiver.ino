3e33c64d3bb069d68a795649c80d78be6a5826cf
silicius <silicius42@gmail.com>
2017-10-16 16:18:55 +0200

    setup function

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,9 +1,22 @@
+#include <qbcan.h>
+
+#define NODE_ID 1
+#define NETWORK_ID 100
+#define ENCRYPT_KEY "tiro finale"
+
+RFM69 radio
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  radio.initialize(FREQUENCY, NODE_ID, NETWORK_ID);
+  radio.setHighPower();
+  radio.encrypt(ENCRYPT_KEY);
+  Serial.println("Done initializing...");
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 
 }
