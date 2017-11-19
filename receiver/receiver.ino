90e3427bb1dd5dc14fd383f6cc8380d2d55f4ab1
Silicius <silicius42@gmail.com>
2017-11-19 22:38:29 +0100

    .

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -9,18 +9,14 @@ RFM69 radio;
 
 void setup() {
   Serial.begin(9600);
-  delay(2000);
+  while(!Serial); // wait for serial to initialize
   Serial.println("Starting initializing...");
 
   radio.initialize(FREQUENCY, NODE_ID, NETWORK_ID);
   radio.setHighPower();
   radio.encrypt(ENCRYPT_KEY);
   Serial.println("...Done initializing");
-  while(!Serial); // wait for serial to initialize
   
-  radio.initialize(FREQUENCY, NODE_ID, NETWORK_ID);
-  radio.setHighPower();
-  radio.encrypt(ENCRYPT_KEY);
   
   Serial.println("Done initializing...");
   delay(1000);
@@ -32,7 +28,6 @@ void loop() {
   
   
   if (radio.receiveDone()) {
-    // print additional info
     unsigned long messageID = arrayToLong(&radio.DATA[0]);
     unsigned long messageTime = arrayToLong(&radio.DATA[4]);
     
@@ -46,13 +41,12 @@ void loop() {
     Serial.print("#["); Serial.print(messageID); Serial.print("][");
     Serial.print(messageDifference); Serial.print("][");
     Serial.print(messageTime); Serial.print("]>>");
+	
     for (byte i=8; i < radio.DATALEN; i++) {
       Serial.print((char)radio.DATA[i]); //print every byte of message
     }
     Serial.print("<<\n");
   }
-  //delay(500);
-
 }
 
 
